c98095db5acca8825d36ade555dc7f2608bfd3f8
Umair Ahmed <umair170@hotmail.com>
2017-10-24 15:00:22 +0100

    Adding a chart feature with typeError

--- src/components/postcodeComponents/graph.js
+++ src/components/postcodeComponents/graph.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React from "react";
+import { Pie } from "react-chartjs-2";
 
-const Graph = () => {
-  return(
-    <div>
-      <h3>Nice graph from somewhere :)</h3>
-    </div>
-  )
+class Graph extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chartData: props.chartData
+    };
+  }
+
+  static defaultProps = {
+    displayTitle: true,
+    displayLegend: true,
+    legendPosition: "right",
+    location: "City"
+  };
+
+  render() {
+    return (
+      <div>
+        <Pie
+          data={this.state.chartData}
+          options={{
+            title: {
+              display: this.props.displayTitle,
+              text: "Largest Cities In " + this.props.location,
+              fontSize: 25
+            },
+            legend: {
+              display: this.props.displayLegend,
+              position: this.props.legendPosition
+            }
+          }}
+        />
+      </div>
+    );
+  }
 }
 
 export default Graph;
