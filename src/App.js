1d4f56830b459b481544ac69d081fcf8944ab345
ahmedu007 <30743917+ahmedu007@users.noreply.github.com>
2017-10-26 10:23:20 +0100

    Adding a navbar with search bar (#7)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import {BrowserRouter, NavLink, Route, Switch} from "react-router-dom";
+import { BrowserRouter, NavLink, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import "./App.css";
-
 
 import Homepage from "./components/Homepage";
 import PostcodePage from "./components/PostcodePage";
@@ -10,20 +10,17 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to Curtain Twitcher</h1>
-        </header>
-        <br />
-        <NavLink to="/">home</NavLink>
-        <br />
-        <NavLink to="/:postcode">postcode</NavLink>
-      <Switch>
-        <Route exact path="/" component={Homepage}/>
-        <Route exact path="/:postcode" component={PostcodePage}/>
-
-      </Switch>
-      </div>
+        <div className="App">
+          <Navbar />
+          <br />
+          <NavLink to="/">home</NavLink>
+          <br />
+          <NavLink to="/:postcode">postcode</NavLink>
+          <Switch>
+            <Route exact path="/" component={Homepage} />
+            <Route exact path="/:postcode" component={PostcodePage} />
+          </Switch>
+        </div>
       </BrowserRouter>
     );
   }
---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Navbar = props => {
+  return (
+    <div className="App">
+      <div className="pos-f-t">
+        <div className="collapse" id="navbarToggleExternalContent">
+          <div className="bg-dark p-4">
+            <h4 className="text-white">Collapsed content</h4>
+            <span className="text-muted">Toggleable via the navbar brand.</span>
+          </div>
+        </div>
+        <nav className="navbar navbar-dark bg-dark justify-content-between">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarToggleExternalContent"
+            aria-controls="navbarToggleExternalContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <form class="form-inline">
+            <input
+              class="form-control mr-sm-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+              Search
+            </button>
+          </form>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
