983fe7a98e9a3ef47acdcf80e8890c9bccb21bdc
ahmedu007 <30743917+ahmedu007@users.noreply.github.com>
2017-10-27 17:23:45 +0100

    Minor fixes to the layout. Adding a FA icon to invalid page (#16)

--- src/components/InvalidPostcode.js
+++ src/components/InvalidPostcode.js
@@ -3,6 +3,9 @@ import React from "react";
 const InvalidPostcode = props => {
   return (
     <div>
+      <br />
+      <i class="fa fa-spinner fa-pulse fa-3x fa-fw" />
+      <span class="sr-only">Loading...</span>
       <p>INVALID POSTCODE, check your facts!!!</p>
     </div>
   );
