8e08a106042c2fc8afb1f845722d6dce240469f4
FrancisBroughton <iamfrancisbroughton@gmail.com>
2017-10-27 12:43:29 +0100

    trends chart now has multiple crimes showing

--- src/components/postcodeComponents/TrendGraph.js
+++ src/components/postcodeComponents/TrendGraph.js
@@ -2,39 +2,43 @@ import React from "react";
 import {Line} from 'react-chartjs-2';
 import TrendsDummy from './graphTrendsDummy';
 
-let dataTrend;
+let dataTrend = [];
 let name;
 const trends = TrendsDummy.map(crime => {
   name = crime.name;
-  dataTrend = Object.values(crime).slice(1)
+  dataTrend.push({[name]: Object.values(crime).slice(1)});
 });
-console.log(trends)
+const dataSetData = dataTrend.map((crime, i) => {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  return {
+    label: Object.keys(crime)[0],
+    fill: false,
+    lineTension: 0.1,
+    backgroundColor: `rgba(${red},${green},${blue},0.4)`,
+    borderColor: `rgba(${red},${green},${blue},1)`,
+    borderCapStyle: 'butt',
+    borderDash: [],
+    borderDashOffset: 0.0,
+    borderJoinStyle: 'miter',
+    pointBorderColor: `rgba(${red},${green},${blue},1)`,
+    pointBackgroundColor: '#fff',
+    pointBorderWidth: 1,
+    pointHoverRadius: 5,
+    pointHoverBackgroundColor: `rgba(${red},${green},${blue},1)`,
+    pointHoverBorderColor: 'rgba(220,220,220,1)',
+    pointHoverBorderWidth: 2,
+    pointRadius: 1,
+    pointHitRadius: 10,
+    data: Object.values(crime)
+  }
+})
+console.log('datasetdata', dataSetData);
 
 const data = {
   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  datasets: [
-    {
-      label: name,
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: dataTrend
-    }
-  ]
+  datasets: dataSetData
 };
 const TrendGraph=(props) => {
   return (
