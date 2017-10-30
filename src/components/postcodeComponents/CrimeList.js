9eb2f68e9aa906a14401ccd11d1cf5d56add162b
FrancisBroughton <iamfrancisbroughton@gmail.com>
2017-10-30 11:46:06 +0000

    filter function added to the crime type

--- src/components/postcodeComponents/CrimeList.js
+++ src/components/postcodeComponents/CrimeList.js
@@ -2,12 +2,26 @@ import React from "react";
 import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
 import dummy from "./graphDataDummy";
 
+const newObj = {}
+const crimeTypes =
+  dummy
+    .map(crime => crime.crimeType)
+    .reduce((acc, crimeType) => {
+      if (!(crimeType in acc)) acc[crimeType] = crimeType;
+      return acc;
+    }, {})
+
+  console.log(crimeTypes, '11111')
+  
+  // "Burglary": "Burglary",
+  // "Other theft": "Other theft"
+  
+
+
 const CrimeChartList = props => {
   return (
-    <BootstrapTable data={dummy} search={true} multiColumnSearch={true}>
-      <TableHeaderColumn isKey dataField="crimeType">
-        Crime Type{" "}
-      </TableHeaderColumn>
+    <BootstrapTable data={dummy}  striped={true} hover={true} pagination={true} >
+    <TableHeaderColumn isKey dataField="crimeType" filter={ { type: 'SelectFilter', options: crimeTypes }} >Crime Type{" "}</TableHeaderColumn>
       <TableHeaderColumn dataField="streetName">Street Name</TableHeaderColumn>
       <TableHeaderColumn dataField="month">Date</TableHeaderColumn>
       <TableHeaderColumn dataField="outcome">Outcome</TableHeaderColumn>
@@ -16,3 +30,9 @@ const CrimeChartList = props => {
 };
 
 export default CrimeChartList;
+
+
+
+
+//search={true} multiColumnSearch={true}
+
