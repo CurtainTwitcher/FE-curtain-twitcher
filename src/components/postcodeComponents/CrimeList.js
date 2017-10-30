622a45cd7df6a67778c4239bac9fcb07eda10182
FrancisBroughton <iamfrancisbroughton@gmail.com>
2017-10-30 12:35:10 +0000

    Feature chart filter (#18)
    * filter function added to the crime type
    
    * columns now in sync with the titles row

--- src/components/postcodeComponents/CrimeList.js
+++ src/components/postcodeComponents/CrimeList.js
@@ -1,18 +1,33 @@
 import React from "react";
 import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
 import dummy from "./graphDataDummy";
+import "./crimeList.css";
+
+const newObj = {}
+const crimeTypes = dummy
+    .map(crime => crime.crimeType)
+    .reduce((acc, crimeType) => {
+      if (!(crimeType in acc)) acc[crimeType] = crimeType;
+      return acc;
+    }, {})
 
 const CrimeChartList = props => {
   return (
-    <BootstrapTable data={dummy} search={true} multiColumnSearch={true}>
-      <TableHeaderColumn isKey dataField="crimeType">
-        Crime Type{" "}
-      </TableHeaderColumn>
-      <TableHeaderColumn dataField="streetName">Street Name</TableHeaderColumn>
-      <TableHeaderColumn dataField="month">Date</TableHeaderColumn>
+    <div>
+    <BootstrapTable data={dummy}  striped={true} hover={true} condensed= {true} pagination={true} >
+    <TableHeaderColumn isKey dataField="crimeType" filter={ { type: 'SelectFilter', options: crimeTypes }} width='300'>Crime Type{" "}</TableHeaderColumn>
+      <TableHeaderColumn dataField="streetName" width='300'>Street Name</TableHeaderColumn>
+      <TableHeaderColumn dataField="month" width='90'>Date</TableHeaderColumn>
       <TableHeaderColumn dataField="outcome">Outcome</TableHeaderColumn>
     </BootstrapTable>
+    </div>
   );
 };
 
 export default CrimeChartList;
+
+
+
+
+//search={true} multiColumnSearch={true}
+
