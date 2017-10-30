99a53bf9e65833aff33382e607c5ef63c1e653b9
FrancisBroughton <iamfrancisbroughton@gmail.com>
2017-10-30 12:10:08 +0000

    columns now in sync with the titles row

--- src/components/postcodeComponents/CrimeList.js
+++ src/components/postcodeComponents/CrimeList.js
@@ -1,31 +1,26 @@
 import React from "react";
 import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
 import dummy from "./graphDataDummy";
+import "./crimeList.css";
 
 const newObj = {}
-const crimeTypes =
-  dummy
+const crimeTypes = dummy
     .map(crime => crime.crimeType)
     .reduce((acc, crimeType) => {
       if (!(crimeType in acc)) acc[crimeType] = crimeType;
       return acc;
     }, {})
 
-  console.log(crimeTypes, '11111')
-  
-  // "Burglary": "Burglary",
-  // "Other theft": "Other theft"
-  
-
-
 const CrimeChartList = props => {
   return (
-    <BootstrapTable data={dummy}  striped={true} hover={true} pagination={true} >
-    <TableHeaderColumn isKey dataField="crimeType" filter={ { type: 'SelectFilter', options: crimeTypes }} >Crime Type{" "}</TableHeaderColumn>
-      <TableHeaderColumn dataField="streetName">Street Name</TableHeaderColumn>
-      <TableHeaderColumn dataField="month">Date</TableHeaderColumn>
+    <div>
+    <BootstrapTable data={dummy}  striped={true} hover={true} condensed= {true} pagination={true} >
+    <TableHeaderColumn isKey dataField="crimeType" filter={ { type: 'SelectFilter', options: crimeTypes }} width='300'>Crime Type{" "}</TableHeaderColumn>
+      <TableHeaderColumn dataField="streetName" width='300'>Street Name</TableHeaderColumn>
+      <TableHeaderColumn dataField="month" width='90'>Date</TableHeaderColumn>
       <TableHeaderColumn dataField="outcome">Outcome</TableHeaderColumn>
     </BootstrapTable>
+    </div>
   );
 };
 
