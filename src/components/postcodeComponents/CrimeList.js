aec9dd6b1d728a0ab76df920092f782cfa41eb94
ahmedu007 <30743917+ahmedu007@users.noreply.github.com>
2017-10-31 12:01:51 +0000

    Adding a tabs feature and minor fixes to the code (#24)

--- src/components/postcodeComponents/CrimeList.js
+++ src/components/postcodeComponents/CrimeList.js
@@ -3,31 +3,43 @@ import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
 import dummy from "./graphDataDummy";
 import "./crimeList.css";
 
-const newObj = {}
 const crimeTypes = dummy
-    .map(crime => crime.crimeType)
-    .reduce((acc, crimeType) => {
-      if (!(crimeType in acc)) acc[crimeType] = crimeType;
-      return acc;
-    }, {})
+  .map(crime => crime.crimeType)
+  .reduce((acc, crimeType) => {
+    if (!(crimeType in acc)) acc[crimeType] = crimeType;
+    return acc;
+  }, {});
 
 const CrimeChartList = props => {
   return (
     <div>
-    <BootstrapTable data={dummy}  striped={true} hover={true} condensed= {true} pagination={true} >
-    <TableHeaderColumn isKey dataField="crimeType" filter={ { type: 'SelectFilter', options: crimeTypes }} width='300'>Crime Type{" "}</TableHeaderColumn>
-      <TableHeaderColumn dataField="streetName" width='300'>Street Name</TableHeaderColumn>
-      <TableHeaderColumn dataField="month" width='90'>Date</TableHeaderColumn>
-      <TableHeaderColumn dataField="outcome">Outcome</TableHeaderColumn>
-    </BootstrapTable>
+      <BootstrapTable
+        data={dummy}
+        striped={true}
+        hover={true}
+        condensed={true}
+        pagination={true}
+      >
+        <TableHeaderColumn
+          isKey
+          dataField="crimeType"
+          filter={{ type: "SelectFilter", options: crimeTypes }}
+          width="300"
+        >
+          Crime Type{" "}
+        </TableHeaderColumn>
+        <TableHeaderColumn dataField="streetName" width="300">
+          Street Name
+        </TableHeaderColumn>
+        <TableHeaderColumn dataField="month" width="90">
+          Date
+        </TableHeaderColumn>
+        <TableHeaderColumn dataField="outcome">Outcome</TableHeaderColumn>
+      </BootstrapTable>
     </div>
   );
 };
 
 export default CrimeChartList;
 
-
-
-
 //search={true} multiColumnSearch={true}
-
