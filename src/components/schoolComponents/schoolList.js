e6e8e6ce1aad19f7f6308bfbeedf1dd370bb8134
Umair Ahmed <umair170@hotmail.com>
2017-11-01 10:06:16 +0000

    Adding icons to school table with sorting functions

--- src/components/schoolComponents/schoolList.js
+++ src/components/schoolComponents/schoolList.js
@@ -13,30 +13,6 @@ class SchoolChartList extends React.Component {
     this.state = {
       arrow: ""
     };
-    this.arrowFinder = this.arrowFinder.bind(this);
-  }
-
-  arrowFinder(cell, row) {
-    // console.log("cell", cell);
-    // console.log("row", row);
-    // for (let i = 0; i < schoolData.length; i++) {
-    //   if (schoolData[i].totalScore < schoolData[i].prevTotalScore) {
-    //     console.log("hello");
-    //     return `<i class="fa fa-arrow-up" aria-hidden="true" /> ${cell}`;
-    //   } else return null;
-    // }
-    let arrow;
-    schoolData.forEach(eachSchool => {
-      if (eachSchool.totalScore <= eachSchool.prevTotalScore) {
-        arrow = `${cell} <i class="fa fa-arrow-up" aria-hidden="true" /> `;
-      } else if (eachSchool.totalScore >= eachSchool.prevTotalScore) {
-        arrow = `${cell} <i class="fa fa-arrow-down" aria-hidden="true" /> `;
-      } else if (eachSchool.totalScore === eachSchool.prevTotalScore) {
-        arrow = `${cell} <i class="fa fa-arrows-h" aria-hidden="true" /> `;
-      }
-    });
-    return arrow;
-    // return `<i class="fa fa-arrow-up" aria-hidden="true" /> ${cell}`;
   }
 
   render() {
@@ -53,38 +29,68 @@ class SchoolChartList extends React.Component {
           dataField="phase"
           filter={{ type: "SelectFilter", options: phases }}
           width="182"
+          dataAlign="center"
         >
           Education
         </TableHeaderColumn>
-        <TableHeaderColumn dataField="name" width="247">
-          School
+        <TableHeaderColumn
+          dataField="name"
+          dataSort
+          width="247"
+          dataAlign="center"
+        >
+          School{<i className="fa fa-sort" aria-hidden="true" />}
         </TableHeaderColumn>
-        <TableHeaderColumn dataField="pupils" width="80">
+        <TableHeaderColumn dataField="pupils" width="80" dataAlign="center">
           Pupils
         </TableHeaderColumn>
-        <TableHeaderColumn dataField="studyProg" width="91">
-          Study Program
+        <TableHeaderColumn
+          dataField="studyProg"
+          dataSort
+          width="91"
+          dataAlign="center"
+        >
+          Study Program{<i className="fa fa-sort" aria-hidden="true" />}
         </TableHeaderColumn>
-        <TableHeaderColumn dataField="earlyYears" width="98">
-          Early years provision
+        <TableHeaderColumn
+          dataField="earlyYears"
+          dataSort
+          width="98"
+          dataAlign="center"
+        >
+          Early years provision{<i className="fa fa-sort" aria-hidden="true" />}
         </TableHeaderColumn>
-        <TableHeaderColumn dataField="outcomes" width="95">
-          Outcome for pupils
+        <TableHeaderColumn
+          dataField="outcomes"
+          dataSort
+          width="95"
+          dataAlign="center"
+        >
+          Outcome for pupils{<i className="fa fa-sort" aria-hidden="true" />}
         </TableHeaderColumn>
-        <TableHeaderColumn dataField="quality" width="117">
-          Quality of teaching, learning and assessment
+        <TableHeaderColumn
+          dataField="quality"
+          dataSort
+          width="117"
+          dataAlign="center"
+        >
+          Quality of teaching, learning and assessment{<i className="fa fa-sort" aria-hidden="true" />}
         </TableHeaderColumn>
-        <TableHeaderColumn dataField="personalDev" width="119">
-          Personal developlent, behaviour and welfare
+        <TableHeaderColumn
+          dataField="personalDev"
+          dataSort
+          width="119"
+          dataAlign="center"
+        >
+          Personal developlent, behaviour and welfare{<i className="fa fa-sort" aria-hidden="true" />}
         </TableHeaderColumn>
         <TableHeaderColumn
           dataField="totalScore"
-          dataFormat={this.arrowFinder}
           dataSort
           width="80"
+          dataAlign="center"
         >
-          {console.log(this.arrowFinder())}
-          Overall Rating
+          Overall Rating{<i className="fa fa-sort" aria-hidden="true" />}
         </TableHeaderColumn>
       </BootstrapTable>
     );
