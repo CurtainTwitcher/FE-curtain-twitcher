aed846bd7efdc8438b3d5bd6ebc3a926a795a62b
Umair Ahmed <umair170@hotmail.com>
2017-12-14 11:03:58 +0000

    Replacing API key to work on deployed version

--- src/components/postcodeComponents/googleMap.js
+++ src/components/postcodeComponents/googleMap.js
@@ -23,7 +23,7 @@ const MyMapComponent = compose(
   ),
   withProps({
     googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places",
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyBkoNI2suDaLFuKebIvMEc2p86Lu33CFx4&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px`, width: `100%` }} />,
     mapElement: <div style={{ height: `100%` }} />
